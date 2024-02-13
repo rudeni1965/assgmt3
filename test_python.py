beaffffb46d12a00e82594e7a67bd2945cbaae3d
Denis <rudeni1965@gmail.com>
2024-02-13 02:12:34 +0100

    first commit

--- test_python.py
+++ test_python.py
@@ -1,8 +1,8 @@
 someNumber = 10
 
-if(someNumber <= 0):
-    print("Over zerro")
+if(someNumber > 0):
+    print("Over zero")
 elif(someNumber < 0):
-    print("Bellow zerro")
+    print("Bellow zero")
 else:
-    print("zerro")
+    print("zero")
